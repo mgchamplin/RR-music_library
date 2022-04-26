afe7dcccf2d8a45b522110ff53dbd1847311c018
Michael Champlin <mchamplin@sbcglobal.net>
2022-04-26 12:24:59 -0700

    Added styling and Context usage

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,7 +1,9 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
+import ButtonStyleContext from "./ButtonStyleContext";
+
 
 function ArtistView() {
     const { id } = useParams()
@@ -24,24 +26,33 @@ function ArtistView() {
         return (
             <div key={i}>
                 <Link to={`/album/${album.collectionId}`}>
-                    <p>{album.collectionName}</p>
+                    <p className="list_style">{album.collectionName}</p>
                 </Link>
             </div>
         )
     })
 
+    const buttonStyling = React.useContext(ButtonStyleContext)
+
     const navButtons = () => {
         return(
             <div>
-                <button onClick={() => navigate(-1)}>Back</button>
-                <button onClick={() => navigate('/')}>Home</button>
+                <button style={buttonStyling} onClick={() => navigate(-1)}>Back</button>
+                <button style={buttonStyling} onClick={() => navigate('/')}>Home</button>
             </div>
         )
+    }
+
+    const artist_style = {
+        marginLeft:"10em",
+        marginRight:"10em",
+        fontSize: "3em",
+        color:"red"
     }
     
     return (
         <div>
-            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {artistData.length > 0 ? <h2 style={artist_style}>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
             {navButtons()}
             {renderAlbums}
         </div>
---
+++ src/components/ButtonStyleContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ButtonStylesContext = React.createContext({});
+
+export default ButtonStylesContext;
